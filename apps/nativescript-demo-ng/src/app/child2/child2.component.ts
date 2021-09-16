335315fe8bbb51d4ab5ad9da91dc3537f76f937b
bentovsky <jpmbento95@gmail.com>
2021-09-16 14:47:40 +0100

    feat: add video recorder plugin

--- apps/nativescript-demo-ng/src/app/child2/child2.component.ts
+++ apps/nativescript-demo-ng/src/app/child2/child2.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { VideoRecorder, Options as VideoRecorderOptions } from '@triniwiz/nativescript-videorecorder';
 
 @Component({
   selector: 'nativescript-child2',
   templateUrl: './child2.component.html',
   styleUrls: ['./child2.component.css'],
 })
-export class Child2Component {}
+export class Child2Component {
+  videorecorder: VideoRecorder;
+
+  constructor() {
+    const options: VideoRecorderOptions = {
+      hd: true,
+      saveToGallery: true,
+    };
+    this.videorecorder = new VideoRecorder(options);
+  }
+
+  onRecord() {
+    this.videorecorder
+      .record()
+      .then((data) => {
+        console.log(data.file);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
